f59e0bdaa58e45bfa46da5de7c7a1cf05249cc6a
mhmmdragilpy <mhmmdragilpy@gmail.com>
2026-05-24 10:13:18 +0700

    fix: scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -20,9 +21,20 @@ export const getProfilPhoto = (name: string) => {
   return match ? (photoModules[match] as string) : '';
 };
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col font-sans">
+      <ScrollToTop />
       <Navbar />
       <main className="flex-grow">
         <Routes>
